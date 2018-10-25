c9ddbdd1e06089757e88712ea9ba679d51769806
24nch3z <sanchezzzgg22@gmail.com>
2018-10-25 14:29:39 +0300

    Rolling Stones

--- app/src/androidTest/java/ru/s4nchez/hackernews/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/s4nchez/hackernews/ExampleInstrumentedTest.kt
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
 
 import org.junit.Assert.*
 
@@ -15,6 +16,7 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
--- app/src/test/java/ru/s4nchez/hackernews/ExampleUnitTest.kt
+++ app/src/test/java/ru/s4nchez/hackernews/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package ru.s4nchez.hackernews
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.mockito.Mockito
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +11,20 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun with_arguments() {
+//        val c = mock(Comparable<*>::class.java)
+//        `when`(c.compareTo("Test")).thenReturn(1)
+//        assertEquals(1, c.compareTo("Test"))
+
+        val str = Mockito.mock(String::class.java)
+        Mockito.`when`(str == "Fuck").thenReturn(true)
+        assertEquals(true, str == "Fuck")
     }
 }
